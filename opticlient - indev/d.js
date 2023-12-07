2d7ab9c28815c0faf280bc1d6ddbb99d26cba982
Izzy <riward1102+opticlient@gmail.com>
2023-12-07 16:39:19 -0500

    .
    .

---
+++ opticlient - indev/d.js
@@ -0,0 +1,15 @@
+//intercept and save bundle.js
+indexedDB.open("opticlient-bundle", 1).addEventListener("success", e => {
+ e = e.target.result;
+ const file = {
+  read: name => {
+   
+  },
+  write: (name, data) => {
+   
+  },
+  exists: name => {
+   
+  },
+ };
+}, { once: true });
